ea86fdf76021cb731fbc43719a06a8128cd8cda2
Aryan <aryan.arora180@gmail.com>
2020-04-20 22:04:21 +0530

    Added OTP phone number authentication

--- app/src/main/java/com/example/bookkaro/loginui/LoginEnterPhoneFragment.kt
+++ app/src/main/java/com/example/bookkaro/loginui/LoginEnterPhoneFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.bookkaro.R
 import com.example.bookkaro.databinding.FragmentLoginEnterPhoneBinding
 
@@ -14,7 +16,13 @@ class LoginEnterPhoneFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentLoginEnterPhoneBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login_enter_phone, container, false)
 
+        binding.loginButton.setOnClickListener {
+            val phone = binding.loginPhoneEdit.text.toString()
 
+            //TODO: Validate phone number and then navigate to validate otp fragment
+            val action = LoginEnterPhoneFragmentDirections.actionLoginEnterPhoneFragmentToLoginValidateOTPFragment(phone)
+            Navigation.findNavController(it).navigate(action)
+        }
 
         return binding.root
     }
