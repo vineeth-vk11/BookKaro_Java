149eb29528dacd7db955297e0ce653ca5b72c8d5
vineeth <f20180233@hyderabad.bits-pilani.ac.in>
2020-04-27 15:31:30 +0530

    Bottom Navigation added in Main Activity

--- app/src/main/java/com/example/bookkaro/MainActivity.java
+++ app/src/main/java/com/example/bookkaro/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.bookkaro;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.bookkaro.mainui.BookingsFragment;
+import com.example.bookkaro.mainui.HelpFragment;
+import com.example.bookkaro.mainui.HomeFragment;
+import com.example.bookkaro.mainui.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch ((item.getItemId())) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_bookings:
+                            selectedFragment = new BookingsFragment();
+                            break;
+                        case R.id.nav_help:
+                            selectedFragment = new HelpFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
+
