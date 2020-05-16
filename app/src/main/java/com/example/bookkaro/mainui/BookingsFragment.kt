c62f663ed7d95c5140025fbbdef5c5c6bd8c89ae
Aryan <aryan.arora180@gmail.com>
2020-05-16 22:15:29 +0530

    Added empty view placeholder for BookingsFragment

--- app/src/main/java/com/example/bookkaro/mainui/BookingsFragment.kt
+++ app/src/main/java/com/example/bookkaro/mainui/BookingsFragment.kt
@@ -21,17 +21,31 @@ class BookingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookings, container, false)
 
+        setNoBookings()
         binding.bookingsRecycler.layoutManager = LinearLayoutManager(requireContext())
 
         val viewModel = ViewModelProvider(this, BookingsViewModelFactory(requireActivity().application)).get(BookingsViewModel::class.java)
         viewModel.getBookings().observe(viewLifecycleOwner, androidx.lifecycle.Observer { bookings ->
             if (!bookings.isNullOrEmpty()) {
+                setBookingsExist()
                 val adapter = BookingsAdapter(bookings, requireContext())
                 binding.bookingsRecycler.adapter = adapter
+            } else {
+                setNoBookings()
             }
         })
 
         return binding.root
+    }
+
+    private fun setNoBookings() {
+        binding.bookingsRecycler.visibility = View.GONE
+        binding.noBookingsText.visibility = View.VISIBLE
+    }
+
+    private fun setBookingsExist() {
+        binding.bookingsRecycler.visibility = View.VISIBLE
+        binding.noBookingsText.visibility = View.GONE
     }
 
 }
