c7eca110cebf509545601ca471c72912b00cc0f5
sakets3010 <sakets3010@gmail.com>
2020-06-02 19:10:31 +0530

    worked out fragments for order and delivery services.

--- app/src/main/java/com/example/bookkaro/mainui/makebooking/delivery/BookDeliveryServicesOrderFragment.kt
+++ app/src/main/java/com/example/bookkaro/mainui/makebooking/delivery/BookDeliveryServicesOrderFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookkaro.R
 import com.example.bookkaro.databinding.BookDeliveryServicesOrderFragmentBinding
+import kotlinx.android.synthetic.main.book_delivery_services_order_fragment.*
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class BookDeliveryServicesOrderFragment : Fragment() {
-
+    private lateinit var dropDownText:AutoCompleteTextView
     private lateinit var binding: BookDeliveryServicesOrderFragmentBinding
 
     private lateinit var viewModel: BookDeliveryServicesOrderViewModel
@@ -24,9 +30,30 @@ class BookDeliveryServicesOrderFragment : Fragment() {
         val args: BookDeliveryServicesOrderFragmentArgs by navArgs()
         val serviceData = args.serviceData
 
-        viewModel = ViewModelProvider(this).get(BookDeliveryServicesOrderViewModel::class.java)
 
-        binding.textView.text = "Fragment: BookDeliveryServicesOrderFragment\nDocument ID: ${serviceData.docId}\nService Requested: ${serviceData.name}"
+
+        viewModel = ViewModelProvider(this).get(BookDeliveryServicesOrderViewModel::class.java)
+        dropDownText=binding.dropdownText
+
+        val items= arrayOf("Stationary")
+
+        val dropDownAdapter:ArrayAdapter<String> = ArrayAdapter(requireContext(),R.layout.list_item,items)
+        dropDownText.setAdapter(dropDownAdapter)
+
+
+
+
+//        val list = mutableListOf<order_list_of_items>()
+//        for (i in 0..4){
+//            list.add(order_list_of_items("item 1"))
+//        }
+//
+//        binding.listOfItemsRecyclerView.apply {
+//            layoutManager=LinearLayoutManager(requireContext())
+//            adapter= listItemsAdapter(list)
+//        }
+
+
 
         return binding.root
     }
