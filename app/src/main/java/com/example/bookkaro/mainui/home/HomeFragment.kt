e0df3a22b0d7fa1be6810ad8c73857ddcbc19fcf
aryanarora180 <aryan.arora180@gmail.com>
2020-07-10 00:09:05 +0530

    Fixed crash for when no address exists

--- app/src/main/java/com/example/bookkaro/mainui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bookkaro/mainui/home/HomeFragment.kt
@@ -47,17 +47,21 @@ class HomeFragment : Fragment() {
 
 
         viewModel.getAddresses().observe(viewLifecycleOwner, Observer { addresses ->
-            val pincode = addresses?.filter { it.default }?.get(0)?.pincode ?: 0L
-            binding.currentAddressText.text = if (pincode != 0L) pincode.toString() else "No address"
-            if (pincode != 0L) {
-                viewModel.getServices(pincode).observe(viewLifecycleOwner, Observer { services ->
-                    if (!services.isNullOrEmpty()) {
-                        binding.servicesRecycler.apply {
-                            layoutManager = LinearLayoutManager(requireContext())
-                            adapter = ServicesGroupAdapter(requireContext(), services, findNavController())
+            if (addresses.isNotEmpty()) {
+                val pincode = addresses?.filter { it.default }?.get(0)?.pincode ?: 0L
+                binding.currentAddressText.text = if (pincode != 0L) pincode.toString() else "No address"
+                if (pincode != 0L) {
+                    viewModel.getServices(pincode).observe(viewLifecycleOwner, Observer { services ->
+                        if (!services.isNullOrEmpty()) {
+                            binding.servicesRecycler.apply {
+                                layoutManager = LinearLayoutManager(requireContext())
+                                adapter = ServicesGroupAdapter(requireContext(), services, findNavController())
+                            }
                         }
-                    }
-                })
+                    })
+                } else {
+                    findNavController().navigate(R.id.action_homeFragment_to_viewAddressFragment)
+                }
             } else {
                 findNavController().navigate(R.id.action_homeFragment_to_viewAddressFragment)
             }
